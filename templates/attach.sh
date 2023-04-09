3e3124b4839a9e1136b21c36d5b2e6d1f67a3c42
Eric Fischer <efischer78@yahoo.fr>
2023-04-09 18:40:10 +0200

    ok

--- templates/attach.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f /opt/xbee/xbeeagent.pid ]; then
-	pid=`cat /opt/xbee/xbeeagent.pid`
-	/root/go/bin/dlv --listen=:{{ .pack.port }} --headless=true --api-version=2 attach $pid
-fi
-
-
